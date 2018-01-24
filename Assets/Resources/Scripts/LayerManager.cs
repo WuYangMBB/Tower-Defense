9ffd4e19eb58243af3f8c26ea1858678609d74a0
spiritiger <spiritiger@users.noreply.github.com>
2018-01-24 22:52:41 +0800

    初版内容pre_alpha，极简的基本流程...
    1.随便拼了个stage
    2.摆了两个center，作为基地和刷小兵用
    3.简单的索敌逻辑，还须可劲儿优化...
    4.加入各个基础类

---
+++ Assets/Resources/Scripts/LayerManager.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LayerManager : MonoBehaviour {
+
+    static public int blueLayer=10;
+    static public int purpleLayer=11;
+
+    static public LayerMask GetEnemyLayer(TEAM team)
+    {
+        LayerMask mask=0;
+        switch (team)
+        {
+            case TEAM.Blue:
+                mask = (1 << purpleLayer);
+                break;
+
+            case TEAM.Purple:
+                mask = (1 << blueLayer);
+                break;
+        }
+       return mask;
+    }
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Assets/Resources/Scripts/StaticUnit/Center.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Center : StaticUnit{
+
+    public GameObject soldier;
+    public GameObject hero;
+    public Transform spawnPoint;
+    public float spawnCoolDown;
+
+    private float timer;
+
+	void Start ()
+    {
+        SpawnSoldier();
+    }
+	
+	void Update ()
+    {
+        timer += Time.deltaTime;
+        if (timer >= spawnCoolDown)
+        {
+            SpawnSoldier();
+            timer = 0;
+        }
+    }
+
+    void SpawnSoldier ()
+    {
+        Instantiate(soldier, spawnPoint.position, spawnPoint.rotation);
+    }
+
+    void SpawnHero ()
+    {
+        Instantiate(hero, spawnPoint.position, spawnPoint.rotation);
+    }
+
+}
+
